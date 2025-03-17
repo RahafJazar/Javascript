f97a88335a6cc85949e4e2023a39c8ddce307320
RahafJazar <rahafjazar73@gmail.com>
2025-03-17 16:25:26 +0300

    week2 & challenge1

---
+++ Elzero/challenge1.js
@@ -0,0 +1,11 @@
+let theTitle = "Elzero", theDescription = "ElzeroWebSchool", theDate = "25/10";
+let card = `
+  <div>
+     <h3> Hello ${theTitle}</h3>
+     <p>${theDescription}</p>
+     <span> ${theDate} </span>
+  </div>
+`;
+
+document.write(card.repeat(4))
+
